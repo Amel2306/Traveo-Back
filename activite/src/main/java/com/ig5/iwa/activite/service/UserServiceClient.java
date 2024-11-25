5b70378dbf2f3260c942ee0cf48892947ae4a095
Amel <ameladdou213@gmail.com>
2024-11-25 11:38:49 +0100

    updating url for user microservice

--- activite/src/main/java/com/ig5/iwa/activite/service/UserServiceClient.java
+++ activite/src/main/java/com/ig5/iwa/activite/service/UserServiceClient.java
@@ -11,7 +11,7 @@ public class UserServiceClient {
     @Autowired
     private RestTemplate restTemplate;
 
-    private static final String USER_SERVICE_URL = "http://usermicroservice:8080/api/user/";
+    private static final String USER_SERVICE_URL = "http://162.38.32.231:8080/api/user/";
 
     public boolean validateUser(Long userId) {
         try {
