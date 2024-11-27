010d70b59ccb15df9660dfd524eb39599f49fc6e
Amel <ameladdou213@gmail.com>
2024-11-27 15:59:06 +0100

    adding ActiviteServiceClient

---
+++ avis/src/main/java/com/ig5/iwa/avis/service/ActiviteServiceClient.java
@@ -0,0 +1,28 @@
+package com.ig5.iwa.avis.service;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+@Service
+public class ActiviteServiceClient {
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    private static final String ACTIVITE_SERVICE_URL = "http://162.38.32.231:8086/api/activities/";
+
+    public boolean validateActivite(Long activiteId) {
+        try {
+            System.out.println("Validating activite with ID: " + activiteId);
+            ResponseEntity<Void> response = restTemplate.getForEntity(ACTIVITE_SERVICE_URL + activiteId, Void.class);
+            System.out.println("Validation successful, response code: " + response.getStatusCode());
+            return true;
+        } catch (Exception e) {
+            System.out.println("Validation failed for activite ID: " + activiteId + ". Error: " + e.getMessage());
+            return false;
+        }
+    }
+}
